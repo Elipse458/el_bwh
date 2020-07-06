8646274a85aade2986b3e735f59618fbf524ae9f
Scullyy <51968381+Scullyy@users.noreply.github.com>
2020-07-06 16:27:28 +0100

    Updated config.lua

--- config.lua
+++ config.lua
@@ -6,9 +6,8 @@ Config.popassistformat = "Player %s is requesting help\nWrite <span class='text-
 Config.chatassistformat = "Player %s is requesting help\nWrite ^2/accassist %s^7 to accept or ^1/decassist^7 to decline\n^4Reason^7: %s" -- chat assist message format
 Config.enable_ban_json = false -- http://<server-ip>:<server-port>/el_bwh/bans.json
 Config.enable_warning_json = false -- http://<server-ip>:<server-port>/el_bwh/warnings.json
-Config.assist_keys = {accept=208,decline=207} -- keys for accepting/declining assist messages (default = page up, page down) - https://docs.fivem.net/game-references/controls/
--- Config.assist_keys = nil -- coment the line above and uncomment this one to disable assist keys
+Config.assist_keys = {enable=true,accept=208,decline=207} -- keys for accepting/declining assist messages (default = page up, page down) - https://docs.fivem.net/game-references/controls/
 Config.warning_screentime = 7.5 * 1000 -- warning display length (in ms)
-Config.backup_kick_method = false -- set this to true if banned players don't get kicked
-Config.discord_webhook = nil -- set to nil to disable, otherwise put "<your webhook url here>" <-- with the quotes!
+Config.backup_kick_method = false -- set this to true if banned players don't get kicked when banned or they can re-connect after being banned.
+Config.discord_webhook = "" -- paste your discord webhook between the quotes if you want to enable discord logs.
 Config.page_element_limit = 250
